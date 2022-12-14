e5a4323583c25a34b567ddfb5e76a3fd17df8774
herdhanur123@gmail.com <herdhanur123@gmail.com>
2022-12-14 14:54:09 +0700

    hero done

--- src/element/Button/index.js
+++ src/element/Button/index.js
@@ -71,6 +71,7 @@ Button.propTypes = {
   target: propTypes.string,
   className: propTypes.string,
   href: propTypes.string,
+  isPrimary: propTypes.bool,
   isDisabled: propTypes.bool,
   isLoading: propTypes.bool,
   isSmall: propTypes.bool,
